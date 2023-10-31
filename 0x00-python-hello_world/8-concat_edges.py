1a35965dd092c378ec9d2a74d5554cf1bf93bc6d
brivan1 <bkryan.bk@gmail.com>
2023-10-31 05:16:34 -0700

    helloworldpy

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
         language that combines remarkable power with very clear syntax"
-        print(str[string.index('object') : string.index('language')-2])
+str = str[39:67] + str[107:112] + str[:6]
+print(str)
